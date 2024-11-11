41fb3deaa49c75e884b7c410b0a87bd893de1924
bekzodbekbozorboyev <bekzodbozorboyev12@gmail.com>
2024-11-11 15:30:53 +0500

    compare chala

--- src/context/store.js
+++ src/context/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { heartSlice } from "./heartSlice";
+import { compareSlice } from "./compareSlice";
 
 export const store = configureStore({
   reducer: {
     heart: heartSlice.reducer,
+    compare: compareSlice.reducer,
   },
 });
